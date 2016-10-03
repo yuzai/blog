c4b59c8f7a76e4d0cb1606723b41d6ff3d70b777
yuzai <a545905403@163.com>
2016-10-03 14:19:32 +0800

    Site updated: 2016-10-03 14:19:28

--- js/category.js
+++ js/category.js
@@ -21,9 +21,21 @@ var head = document.getElementById('daohang');
   else {
       head.style.opacity = 0.4;
   }
-  if(window.pageYOffset >= 280)
+  if(window.pageYOffset >= 230 && window.innerWidth >=768 ){
+    var w = $('#ab_ca').parent().width();
     $('#ab_ca').addClass('about_fix');
+    $('#ab_ca').css('width',w);
+  }
   else {
     $('#ab_ca').removeClass('about_fix');
   }
+})
+
+document.getElementById('daohang').addEventListener('mouseover',function(){
+var head = document.getElementById('daohang');
+      head.style.opacity = 1;
+})
+document.getElementById('daohang').addEventListener('mouseout',function(){
+var head = document.getElementById('daohang');
+      head.style.opacity = 0.4;
 })
