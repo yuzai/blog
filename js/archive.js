9318546a3d7d297217021bbfcedc33a65f4e1a64
yuzai <a545905403@163.com>
2016-10-03 18:01:24 +0800

    Site updated: 2016-10-03 18:01:22

--- js/archive.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-var list = document.getElementsByClassName('category-list-count');
-for(var i=0;i<list.length;i++)
-{
-  list[i].innerHTML = '('+this.innerHTML+')';
-}
-}
-)
