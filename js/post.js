d4b1fcbf3d006387b57f4964041d2f2b9728e7d4
yuzai <a545905403@163.com>
2016-10-04 18:09:59 +0800

    Site updated: 2016-10-04 18:09:55

--- js/post.js
+++ js/post.js
@@ -1,3 +1,13 @@
+var list = document.getElementsByClassName('category-list-count');
+for(var i=0;i<list.length;i++)
+{
+    list[i].innerHTML  = ' ('+list[i].innerHTML+')';
+}
+var list = document.getElementsByClassName('archive-list-count');
+for(var i=0;i<list.length;i++)
+{
+    list[i].innerHTML  = ' ('+list[i].innerHTML+')';
+}
 document.addEventListener('scroll',function(){
 var head = document.getElementById('daohang');
   if(window.pageYOffset !== 0)
@@ -5,7 +15,7 @@ var head = document.getElementById('daohang');
   else {
       head.style.opacity = 0.4;
   }
-  if(window.pageYOffset >= 230 && window.innerWidth >=768 ){
+  if(window.pageYOffset >= 202 && window.innerWidth >=768 ){
     var w = $('#ab_ca').parent().width();
     $('#ab_ca').addClass('about_fix');
     $('#ab_ca').css('width',w);
@@ -14,3 +24,16 @@ var head = document.getElementById('daohang');
     $('#ab_ca').removeClass('about_fix');
   }
 })
+var link = document.getElementsByClassName('toc-link');
+for(var i=0;i<link.length;i++)
+{
+    var mao = /#[\u4e00-\u9fa5]*/;
+    console.log(link[i].href.match(mao)[0]);
+    link[i].onclick = function(){
+      var temp = $(this.href.match(mao)[0]);
+      $(this.href.match(mao)[0]).animate({'font-size':'30px'},1000);
+      $(this.href.match(mao)[0]).css('color','green');
+      $('html,body').animate({scrollTop:$(this.href.match(mao)[0]).offset().top-100},500);
+      return false;
+    }
+}
